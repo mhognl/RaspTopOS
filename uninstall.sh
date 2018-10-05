873fdd29f0563a99f72259c58563c7304ab14672
mhognl <43824816+mhognl@users.noreply.github.com>
2018-10-05 15:39:30 +0200

    Install - uninstall scripts

---
+++ uninstall.sh
@@ -0,0 +1,7 @@
+if [ ! "`whoami`" = "root" ]
+then
+    echo "You must run this script as root."
+    exit 1
+fi
+
+ping google.com -c 2 && apt purge git apache2 openssh-server sudo net-tools -y && rm -rf /etc/sudoers.d/web /etc/apache2 /usr/lib/cgi-bin /os /recovery
