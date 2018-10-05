e0852caaa139026cabea025e6dea23bd320c025d
mhognl <43824816+mhognl@users.noreply.github.com>
2018-10-05 19:28:08 +0200

    Delete uninstall.sh

--- uninstall.sh
+++
@@ -1,7 +0,0 @@
-if [ ! "`whoami`" = "root" ]
-then
-    echo "You must run this script as root."
-    exit 1
-fi
-
-ping google.com -c 2 && apt purge git apache2 openssh-server net-tools -y && rm -rf /etc/sudoers.d/web /etc/apache2 /usr/lib/cgi-bin /os /recovery
