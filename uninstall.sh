1d8d7995e69d48eb0588bb37161ce04a37a6d341
mhognl <43824816+mhognl@users.noreply.github.com>
2019-04-16 16:05:56 +0200

    Updated uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -4,6 +4,6 @@ then
     exit 1
 fi
 
-apt purge git apache2 libapache2-mod-perl2 libcgi-session-perl libapache-htpasswd-perl php ufw -y
+apt purge git apache2 libapache2-mod-perl2 libcgi-session-perl libapache-htpasswd-perl php -y
 apt autoremove -y
 rm -rf /etc/sudoers.d/web /etc/apache2 /os /recovery
